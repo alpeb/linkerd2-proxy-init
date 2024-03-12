d81f59e9c08297c9797b6658359c81c734fbf250
Alejandro Pedraza <alejandro@buoyant.io>
2024-03-12 05:12:50 -0500

    test4

--- proxy-init/integration/run.sh
+++ proxy-init/integration/run.sh
@@ -39,38 +39,6 @@ echo "POD_WITH_NO_RULES_IP=${POD_WITH_NO_RULES_IP}"
 POD_WITH_EXISTING_RULES_IP=$(kip pod-with-existing-rules)
 echo "POD_WITH_EXISTING_RULES_IP=${POD_WITH_EXISTING_RULES_IP}"
 
-POD_REDIRECTS_ALL_PORTS_IP=$(kip pod-redirects-all-ports)
-echo "POD_REDIRECTS_ALL_PORTS_IP=${POD_REDIRECTS_ALL_PORTS_IP}"
-
-POD_REDIRECTS_WHITELISTED_IP=$(kip pod-redirects-whitelisted)
-echo "POD_REDIRECTS_WHITELISTED_IP=${POD_REDIRECTS_WHITELISTED_IP}"
-
-POD_DOESNT_REDIRECT_BLACKLISTED_IP=$(kip pod-doesnt-redirect-blacklisted)
-echo "POD_DOESNT_REDIRECT_BLACKLISTED_IP=${POD_DOESNT_REDIRECT_BLACKLISTED_IP}"
-
-POD_IGNORES_SUBNETS_IP=$(kip pod-ignores-subnets)
-echo "POD_IGNORES_SUBNETS_IP=${POD_IGNORES_SUBNETS_IP}"
-
 k get po -A -owide
 
-echo '# Running tester...'
-k run iptables-tester \
-        --attach \
-        --command \
-        --env=POD_IGNORES_SUBNETS_IP="${POD_IGNORES_SUBNETS_IP}" \
-        --env=POD_REDIRECTS_ALL_PORTS_IP="${POD_REDIRECTS_ALL_PORTS_IP}" \
-        --env=POD_REDIRECTS_WHITELISTED_IP="${POD_REDIRECTS_WHITELISTED_IP}" \
-        --env=POD_DOESNT_REDIRECT_BLACKLISTED_IP="${POD_DOESNT_REDIRECT_BLACKLISTED_IP}" \
-        --env=POD_WITH_EXISTING_RULES_IP="${POD_WITH_EXISTING_RULES_IP}" \
-        --env=POD_WITH_NO_RULES_IP="${POD_WITH_NO_RULES_IP}" \
-        --image=test.l5d.io/linkerd/iptables-tester:test \
-        --image-pull-policy=Never \
-        --namespace=proxy-init-test \
-        --quiet \
-        --restart=Never \
-        --rm \
-        -- \
-        go test -integration-tests
-
-
-#k delete ns proxy-init-test
+k -n proxy-init-test logs pod-with-existing-rules linkerd-init
