5a5ee1586e3a38403f6e66357f87709ca41e93c2
Alejandro Pedraza <alejandro@buoyant.io>
2024-03-12 07:45:16 -0500

    test14

--- proxy-init/integration/run.sh
+++ proxy-init/integration/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euxo pipefail
 
 cd "${BASH_SOURCE[0]%/*}"
 
@@ -30,18 +30,4 @@ if k get ns/proxy-init-test >/dev/null 2>&1 ; then
 fi
 
 echo '# Creating the test lab...'
-cat /proc/net/if_inet6
-modinfo ipv6
-lsmod | grep -i ipv6
-k create ns proxy-init-test
-k create -f iptables/iptablestest-lab.yaml
-
-POD_WITH_NO_RULES_IP=$(kip pod-with-no-rules)
-echo "POD_WITH_NO_RULES_IP=${POD_WITH_NO_RULES_IP}"
-
-POD_WITH_EXISTING_RULES_IP=$(kip pod-with-existing-rules)
-echo "POD_WITH_EXISTING_RULES_IP=${POD_WITH_EXISTING_RULES_IP}"
-
-k get po -A -owide
-k -n proxy-init-test describe po pod-with-existing-rules
-k -n proxy-init-test logs pod-with-existing-rules iptables-test
+sysctl -a | grep disable_ipv6
